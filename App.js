e6428aeaa7ba0b775a95ebad5de6cb68df371b32
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-11-19 19:36:22 +0100

    Structure App Layers

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.boxA}>
+        <Text style={styles.textLogo}>Soora</Text>
+        <Button title='Contact us' />
+      </View>
+      <View style={styles.boxB}>
+        <Text style={styles.textCoomingSoon}>Comming Soon</Text>
+        <Text style={styles.textHeroTitle}>Bringing Muslims Together</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="here!..." style={styles.TextInput} />
+        <Button title='click here!' />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 10
   },
+  boxA:{
+    backgroundColor: 'yellow',
+  },
+  textLogo: {
+    fontSize: 16
+  },
+  textCoomingSoon: {
+    color: 'blue',
+  },
+  textHeroTitle: {
+    fontSize: 20,
+  },
+  boxB:{
+    backgroundColor: 'pink',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '60%'
+  }
 });
