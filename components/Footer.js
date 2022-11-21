6a5477c1252c3088076f9b6fc55487e0d2cf7d3c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-11-21 20:19:33 +0100

    Implement footer section

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
 
 function Footer() {
   return (
     <View style={styles.footerContainer}>
       <View style={styles.footerSocialMedia}>
-        <Text>Facebook Twitter</Text>
+        <TouchableOpacity style={styles.footerIconBox}>
+          <Image
+            source={require('../assets/images/facebook.png')}
+            style={styles.footerIconStyle}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIconBox}>
+          <Image
+            source={require('../assets/images/twitter.png')}
+            style={styles.footerIconStyle}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIconBox}>
+          <Image
+            source={require('../assets/images/instagram.png')}
+            style={styles.footerIconStyle}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIconBox}>
+          <Image
+            source={require('../assets/images/tiktok.png')}
+            style={styles.footerIconStyle}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIconBox}>
+          <Image
+            source={require('../assets/images/youtube.png')}
+            style={styles.footerIconStyle}
+          />
+        </TouchableOpacity>
       </View>
       <View style={styles.footerCopyrights}>
-        <Text>Copyright 2022 Soora. All rights reserved</Text>
+        <Text>{"Copyright \u00a9 2022 Soora. All rights reserved"}</Text>
       </View>
    </View>
   )
@@ -20,6 +49,26 @@ const styles = StyleSheet.create({
   },
   footerSocialMedia: {
     backgroundColor: 'red',
+    paddingTop: 135,
+    paddingBottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  footerIconBox: {
+    backgroundColor: 'powderblue',
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 100,
+    marginHorizontal: 5,
+  },
+  footerIconStyle: {
+    resizeMode: 'contain',
+    width: 24,
+    height: 24,
   },
   footerCopyrights: {
     backgroundColor: 'green',
