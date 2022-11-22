6686e7b0c93023e4d4c8f710ab764896f66b0aff
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2022-11-22 22:06:23 +0100

    Update App component

--- App.js
+++ App.js
@@ -9,34 +9,37 @@ import { StyleSheet, SafeAreaView, FlatList} from 'react-native';
 // import ContactUsButton from './components/ContactUsButton';
 // import BoxStoresImages from './components/BoxStoresImages';
 // import InputEmail from './components/InputEmail';
-import Footer from './components/Footer';
 import BoxNavigation from './views/BoxNavigation';
 import BoxDisplay1 from './views/BoxDisplay1';
 import BoxImageA from './views/BoxImageA';
 import BoxDisplay2 from './views/BoxDisplay2';
 import BoxInput from './views/BoxInput';
 import BoxImageB from './views/BoxImageB';
+import BoxFooter from './views/BoxFooter';
 
 
 export default function App() {
 
   const componentsList = [
-    <BoxNavigation />,
-    <BoxDisplay1 />,
-    <BoxImageA />,
-    <BoxDisplay2 />,
-    <BoxInput />,
-    <BoxImageB />,
-    <Footer />
+    // { id: 0, compo: <BoxNavigation />},
+    // { id: 1, compo: <BoxDisplay1 />},
+    // { id: 2, compo: <BoxImageA />},
+    // { id: 4, compo: <BoxInput />},
+    // { id: 5, compo: <BoxDisplay2 />},
+    // { id: 6, compo: <BoxImageB />},
+    { id: 7, compo: <BoxFooter />},
   ];
 
   return (
     <SafeAreaView style={styles.appContainer}>
       <FlatList
         data={componentsList}
+        keyExtractor = {item => item.id}
         renderItem={
-          (item) => {
-            return item
+          ({item}) => {
+            return(
+              <>{item.compo}</>
+            )
           }
         }
       />
